73d9c446bff77e684b317481ad6030ea36a31f08
davidelockwood780 <davidelockwood780@gmail.com>
2025-03-13 10:48:22 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,10 @@
+ <?php
+    $x = rand(0, 10);
+    if ($x < 5) {
+        echo "The number is less than 5";
+    } elseif ($x == 5) {
+        echo "The number is equal to 5";
+    } else {
+        echo "The number is greater than 5";
+    }
+?>
