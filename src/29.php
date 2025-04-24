a2232f008d5ac000969f1bd1683ef532ee15eff6
davidelockwood780 <davidelockwood780@gmail.com>
2025-04-24 15:19:20 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,10 @@
+<?php
+function echo_random_string($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $random_string = '';
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, strlen($characters)-1)];
+    }
+    return $random_string;
+}
+echo_random_string(10);
